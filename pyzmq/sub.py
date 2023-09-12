14ab6f8e1b47771a0a18755a93d8b5a1f685e685
mikst <mika@kobakant.at>
2023-09-12 20:28:20 +0200

    zmq communication fix

--- pyzmq/sub.py
+++ pyzmq/sub.py
@@ -1,9 +1,9 @@
 import zmq
-xpub_addr = 'tcp://192.168.0.10:5555'
+xpub_addr = 'tcp://192.168.0.100:5555'
 context = zmq.Context()
 subscriberSocket = context.socket(zmq.SUB)
 subscriberSocket.connect(xpub_addr)
-subscriberSocket.setsockopt(zmq.SUBSCRIBE, b"sensors")
+subscriberSocket.setsockopt(zmq.SUBSCRIBE, b"control")
 while True:
     if subscriberSocket.poll(timeout=1000):
         message = subscriberSocket.recv_multipart()
