d01f1ff3a6382d2e882e96632f9b131ee3460275
Ingo Randolf <ingo.randolf@servus.at>
2023-10-01 17:49:22 +0200

    remotell: use StrictHostKeyChecking=no, improve logging and usage

--- scripts/remotell.sh
+++ scripts/remotell.sh
@@ -2,27 +2,35 @@
 
 PROJECT_FOLDER=/root/Bela/projects/
 IP_PREFIX="192.168.0."
+IP_OFFSET=100
+
+
+print_udage () {
+    echo
+    echo "usage:"
+    echo "remotell.sh id project_folder"
+    echo
+    echo "id:               device-id (1..99)"
+    echo "project_folder:   optional"
+    echo
+}
 
 
 if [ -z ${1+x} ]; then
-    echo "no id set.
-usage: ll.sh <ID> <project folder>
-"
-    echo "ID: 1..99 (mandatory)"
-    echo "the project folder is optional
-    "
+    echo "invalid range: no id set"
+    print_udage
     exit
 fi
 
-ID=$(($1 + 100))
+ID=$(($1 + IP_OFFSET))
 
 if (( ID < 101 )); then
-    echo "invalid range. id must be >= 1"
+    echo "invalid range: id must be >= 1"
     exit
 fi
 
 if (( ID >= 200 )); then
-    echo "invalid range. id must be < 100"
+    echo "invalid range: id must be < 100"
     exit
 fi
 
@@ -31,7 +39,7 @@ IP=${IP_PREFIX}${ID}
 RES=`ping -o -t 1 -W 1 -c 1 ${IP}`
 
 if (( $? == 0 )); then
-    ssh root@${IP} "ls -l ${PROJECT_FOLDER}${2}"
+    ssh -o StrictHostKeyChecking=no root@${IP} "ls -l ${PROJECT_FOLDER}${2}"
 else
     echo "could not ping ${IP}"
 fi
