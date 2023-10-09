b35ee10716284c24f5ae5a047e24b5f4cbe392c1
Ingo Randolf <ingo.randolf@servus.at>
2023-10-09 20:45:16 +0200

    pass block-size to run_project, add restart_symphony

---
+++ scripts/restart_symphony.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./stop_symphony.sh $@
+./start_symphony.sh $@
--- scripts/start_symphony.sh
+++ scripts/start_symphony.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./remote_command.sh $@ /root/Bela/scripts/run_project.sh -b symphony
+./remote_command.sh $@ /root/Bela/scripts/run_project.sh -b symphony -c \"-p 2048\"
